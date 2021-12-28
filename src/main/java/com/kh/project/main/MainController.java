7998d69d83922127b3ac9a13a46f3a8368cd311f
smnnnn <humminghumming@naver.com>
2021-12-28 20:22:36 +0900

    lombok dependency 추가, port 설정

---
+++ src/main/java/com/kh/project/main/MainController.java
@@ -0,0 +1,20 @@
+package com.kh.project.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping(value= {"/", "/main"})
+	public String main() {
+		return "main/main";
+	}
+	
+	/* 루트로 포워딩 하더라도 처리(main으로 이동)되게끔 하는 메소드 */
+	@PostMapping(value="/")
+	public String redirectMain() {
+		return "redirect:/";
+	}
+}
