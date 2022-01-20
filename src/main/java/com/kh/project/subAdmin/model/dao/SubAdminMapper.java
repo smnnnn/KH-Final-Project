2dff37804d45fb1096625ce52c7ecbbdc26897ed
HyoEun-L <eunlee0510@gmail.com>
2022-01-21 08:04:00 +0900

    의료진 등록 로직 수정

--- src/main/java/com/kh/project/subAdmin/model/dao/SubAdminMapper.java
+++ src/main/java/com/kh/project/subAdmin/model/dao/SubAdminMapper.java
@@ -6,17 +6,21 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.kh.project.subAdmin.model.vo.Holiday;
 import com.kh.project.subAdmin.model.vo.Veterinarian;
+import com.kh.project.subAdmin.model.vo.VeterinarianAndHoliday;
 
 @Mapper
 public interface SubAdminMapper {
+	
+	int modifyStatusVeterinarian(int tno);
 
 	int registVeterinarian(Veterinarian newVeterinarian);
 	
 	int registHoliday(Holiday holiday);
 
-	List<Veterinarian> findVeterinarian(String vname);
+	List<VeterinarianAndHoliday> findVeterinarian(String vname);
 
 	int modifyVeterinarian(Veterinarian veterinarianModify);
 
 	int modifyHoliday(Holiday holiday);
+
 }
